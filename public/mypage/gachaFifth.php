31cd8f4d3a51301aeda85d89f3db525ed96333fa
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2016-05-29 19:07:02 +0900

    gachaFifth修正

--- public/mypage/gachaFifth.php
+++ public/mypage/gachaFifth.php
@@ -46,23 +46,25 @@ foreach($gacha_items as $value) {
     }
 }
 
-// がちゃログにインサート
-try{
-    $sql = 'INSERT INTO `gacha_log` VALUES (null, :user_id, :gacha_log_number, NOW(), NOW())';
-    $prepare = $db->prepare($sql);
-    $prepare->bindValue(':user_id', $_SESSION['user_id']);
-    $prepare->bindValue(':gacha_log_number', time());
-    $prepare->execute();
+try {
+    $sql_log = 'INSERT INTO `gacha_log` VALUES (null, :user_id, :time, :time, :time)';
+    $prepare_log = $db->prepare($sql_log);
+    $prepare_log->bindValue(':user_id', $_SESSION['user_id']);
+    $prepare_log->bindValue(':time', time());
+    $prepare_log->execute();
 
-    $sql = 'INSERT INTO `user_item` VALUES(null, :user_id, :item_id, NOW(), NOW())';
-    $prepare = $db->prepare($sql);
-    $prepare->bindValue(':user_id', $_SESSION['user_id']);
-    $prepare->bindValue(':item_id', $item_id);
-    $prepare->execute();
-} catch {
+    $sql_item = 'INSERT INTO `user_item` VALUES(null, :user_id, :item_id, :time, :time)';
+    $prepare_item = $db->prepare($sql_item);
+    $prepare_item->bindValue(':user_id', $_SESSION['user_id']);
+    $prepare_item->bindValue(':item_id', $item_id);
+    $prepare_item->bindValue(':time', time());
+    $prepare_item->execute();
+
+} catch (PDOException $e) {
     $_SESSION['error_flg'] = "データベース接続エラー②";
     header('Location: ./mypage.php');
     exit;
 }
+
 header('Location: ./result.php');
-        exit;
+exit;
