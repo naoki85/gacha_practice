1e36cf5d5c766ace4d7c8135ec0858c706715d21
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2016-05-29 21:51:10 +0900

    test

--- public/mypage/gachaFifth.php
+++ public/mypage/gachaFifth.php
@@ -10,13 +10,13 @@ $gacha_items = $prepare->fetchAll();
 
 $max = 0;
 foreach($gacha_items as $value) {
-    $max += $value['rate'];
+    $max += $value['ratio'];
 }
 
 $point = mt_rand(0, $max - 1);
 
 foreach($gacha_items as $value) {
-    $max -= $value['rate'];
+    $max -= $value['ratio'];
     if($max <= $point) {
         $item_id = $value['item_id'];
         $_SESSION['result'] = $value['item_image'];
@@ -25,18 +25,17 @@ foreach($gacha_items as $value) {
 }
 //var_dump($_SESSION['result']);
 
-$sql_log = 'INSERT INTO `gacha_log` VALUES (null, 3, "aaaaa",null,null)';
+$sql_log = 'INSERT INTO `gacha_log` VALUES (null, :user_id, "test", :time, :time)';
 $prepare_log = $db->prepare($sql_log);
-
-//$prepare_log->bindValue(':user_id', $_SESSION['user_id']);
-//$prepare_log->bindValue(':time', time());
+$prepare_log->bindValue(':user_id', $_SESSION['user_id']);
+$prepare_log->bindValue(':time', time());
 $prepare_log->execute();
 
-$sql_item = 'INSERT INTO `user_item` VALUES(null, 1, 2, null, null)';
+$sql_item = 'INSERT INTO `user_item` VALUES(null, :user_id, :item_id, :time, :time)';
 $prepare_item = $db->prepare($sql_item);
-//$prepare_item->bindValue(':user_id', $_SESSION['user_id']);
-//$prepare_item->bindValue(':item_id', $item_id);
-//$prepare_item->bindValue(':time', time());
+$prepare_item->bindValue(':user_id', $_SESSION['user_id']);
+$prepare_item->bindValue(':item_id', $item_id);
+$prepare_item->bindValue(':time', time());
 $prepare_item->execute();
 
 header('Location: ./result.php');
