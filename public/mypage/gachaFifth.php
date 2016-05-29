36e12e8d448938a13050b8c82a8a925534c04432
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2016-05-29 19:21:37 +0900

    gachaFifth修正

--- public/mypage/gachaFifth.php
+++ public/mypage/gachaFifth.php
@@ -1,33 +1,10 @@
 <?php
+require_once '../template/db.php';
 
-session_start();
-
-// ログインチェック
-session_start();
-if(!$_SESSION['user_id']) {
-    return header('Location: ../login/login.php');
-}
-
-$cleardb = parse_url(getenv('CLEARDB_DATABASE_URL'));
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-try {
-    $db = new PDO(sprintf("mysql:dbname=%s;host=%s", substr($cleardb['path'], 1), $cleardb['host']), $cleardb['user'], $cleardb['pass']);
-    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $sql = 'SELECT * FROM `gacha` INNER JOIN `item` ON `gacha`.`item_id` = `item`.`id`';
-    $prepare = $db->prepare($sql);
-    $prepare->execute();
-    $gacha_items = $prepare->fetchAll();
-
-} catch (PDOException $e) {
-    $_SESSION['error_flg'] = "データベース接続エラー";
-    header('Location: ../mypage/mypage.php');
-    exit;
-}
+$sql = 'SELECT * FROM `gacha` INNER JOIN `item` ON `gacha`.`item_id` = `item`.`id`';
+$prepare = $db->prepare($sql);
+$prepare->execute();
+$gacha_items = $prepare->fetchAll();
 
 // 以下、がちゃ④と同様の実装
 $max = 0;
@@ -46,25 +23,19 @@ foreach($gacha_items as $value) {
     }
 }
 
-try {
-    $sql_log = 'INSERT INTO `gacha_log` VALUES (null, :user_id, :time, :time, :time)';
-    $prepare_log = $db->prepare($sql_log);
-    $prepare_log->bindValue(':user_id', $_SESSION['user_id']);
-    $prepare_log->bindValue(':time', time());
-    $prepare_log->execute();
 
-    $sql_item = 'INSERT INTO `user_item` VALUES(null, :user_id, :item_id, :time, :time)';
-    $prepare_item = $db->prepare($sql_item);
-    $prepare_item->bindValue(':user_id', $_SESSION['user_id']);
-    $prepare_item->bindValue(':item_id', $item_id);
-    $prepare_item->bindValue(':time', time());
-    $prepare_item->execute();
+$sql_log = 'INSERT INTO `gacha_log` VALUES (null, :user_id, :time, :time, :time)';
+$prepare_log = $db->prepare($sql_log);
+$prepare_log->bindValue(':user_id', $_SESSION['user_id']);
+$prepare_log->bindValue(':time', time());
+$prepare_log->execute();
 
-} catch (PDOException $e) {
-    $_SESSION['error_flg'] = "データベース接続エラー②";
-    header('Location: ./mypage.php');
-    exit;
-}
+$sql_item = 'INSERT INTO `user_item` VALUES(null, :user_id, :item_id, :time, :time)';
+$prepare_item = $db->prepare($sql_item);
+$prepare_item->bindValue(':user_id', $_SESSION['user_id']);
+$prepare_item->bindValue(':item_id', $item_id);
+$prepare_item->bindValue(':time', time());
+$prepare_item->execute();
 
 header('Location: ./result.php');
 exit;
