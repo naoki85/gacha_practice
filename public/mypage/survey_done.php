29b22e01e922e57b1e464e7cbaf4af90a59bfd98
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2016-06-04 01:43:52 +0900

    survey

--- public/mypage/survey_done.php
+++ public/mypage/survey_done.php
@@ -47,7 +47,11 @@ require_once '../template/template_header.php';
 ?>
 
 <!-- 本体 -->
-
+<?php if($survey_flg == 1): ?>
+    <h3>ご回答ありがとうございました！</h3>
+<?php else: ?>
+    <h3>既にご回答いただいております。ありがとうございました！</h3>
+<?php endif; ?>
 
 <?php
 require_once '../template/template_footer.php';
